5d8fd3600ec577797db6db7ad5f5d3e31576505f
samfr7 <samuelfrank9486194731@gmail.com>
2026-03-08 23:57:35 +0530

    Increase password_hash field length to 255 characters for improved security

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
     todos = db.relationship('Todo', backref='user', lazy=True)
 
